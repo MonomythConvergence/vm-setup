1fe6d727d707fb797346974619398896a5b24fd7
MonomythConvergence <136901543+MonomythConvergence@users.noreply.github.com>
2025-06-10 16:30:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# 1. Ensure your user is in vboxsf group
-sudo usermod -aG vboxsf $USER
-
-# 2. Set default permissions for ALL shared folders
-echo "options vboxsf uid=$(id -u),gid=$(id -g),dmode=775,fmode=664" | \
-sudo tee -a /etc/modprobe.d/vboxsf.conf
+sudo chown -R $USER:vboxsf /media/sf_*
+sudo chmod -R 775 /media/sf_*
